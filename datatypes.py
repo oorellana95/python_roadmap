019fcb4334d5a46fc6bf477eeed8ee9cf8b82ea4
oorellana95 <oorellana30.95@gmail.com>
2023-08-31 19:57:41 -0500

    add datatypes

---
+++ datatypes.py
@@ -0,0 +1,17 @@
+# Lists are just like dynamic-sized arrays,
+simple_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+empty_list = []
+# A Tuple is similar to a list in terms of indexing, nested objects, and repetition but a tuple is immutable
+simple_tuple = (1, 2, 3, 4, 5, 6, 7)
+empty_tuple = ()
+
+# A Set is an unordered collection data type that is iterable, mutable and has no duplicate elements and
+# it is not subscriptable (we can not access its elements directly)
+simple_set = {1, 2, 3, 4, 5, 6, 7, 8}
+empty_set = set()
+
+# A Dictionary holds key:value pair. It is an ordered (since Py 3.7) [unordered (Py 3.6 & prior)] collection of data values
+simple_dic = {1: 'Monday', 2: 'Tuesday'}
+empty_dic = {}
+
+print(empty_set)
