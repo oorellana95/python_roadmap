61af0417ee10f2b9b865585411436136a1c454d8
oorellana95 <oorellana30.95@gmail.com>
2023-09-02 22:33:57 -0500

    Add queue class

---
+++ linked_lists/queue.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        # Its length drops to zero after consuming all elements in a loop, even in a simple print()
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
