3ace4c35c6527cce297ade4a47ae5df519e3f7fd
oorellana95 <oorellana30.95@gmail.com>
2023-09-02 23:10:39 -0500

    Add priority queues

--- linked_lists/queues.py
+++ linked_lists/queues.py
@@ -1,17 +1,20 @@
 from collections import deque
+from heapq import heappop, heappush
+from itertools import count
 
 
-class Queue:
-    def __init__(self, *elements):
-        self._elements = deque(elements)
-
+class IterableMixin:
     def __len__(self):
         return len(self._elements)
 
     def __iter__(self):
-        # Its length drops to zero after consuming all elements in a loop, even in a simple print()
         while len(self) > 0:
             yield self.dequeue()
+
+
+class Queue(IterableMixin):
+    def __init__(self, *elements):
+        self._elements = deque(elements)
 
     def enqueue(self, element):
         self._elements.append(element)
@@ -23,3 +26,42 @@ class Queue:
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
+
+
+class PriorityQueueMinHeap(IterableMixin):
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)
+
+
+class PriorityQueueMaxHeap(IterableMixin):
+    """
+    Implementation example:
+
+    CRITICAL = 3
+    IMPORTANT = 2
+    NEUTRAL = 1
+
+    messages = PriorityQueue()
+    messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+    messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+    messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+    messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+    messages.dequeue()
+    # 'Brake pedal depressed'
+    """
+    def __init__(self):
+        self._elements = []
+        self._counter = count()  # we could avoid creating the _counter if we add a timestamp below.
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
